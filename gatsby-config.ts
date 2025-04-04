91209a635972b6e3cbe3d368d22693a9564e3ae6
vlad-l8 <111335294+Vlad-L8@users.noreply.github.com>
2025-04-04 12:14:13 +0400

    removed prefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,6 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  pathPrefix: "/vig-ash",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
